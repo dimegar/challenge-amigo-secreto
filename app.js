9c84ab047ccb9b3985712c3e44ecb1c9c4f0788d
dimegar <mendez.garcia.diego@gmail.com>
2025-08-20 16:16:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 // Creando el array para almacenar los nombres.
 let amigos = [];
-
+//Mostrar lista de amigos
+function listaAmigos(elemento,texto){
+    let elemntoHTML = document.querySelector(elemento);
+    elemntoHTML.innerHTML = texto;
+    return;
+} 
 //Interacción con añadir
 function agregarAmigo(){
     let amigoIngresado = document.getElementById('amigo').value;
+    if (amigoIngresado == ''){
+        alert("Inserte un nombre.");
+    }
+    else{
     amigos.push(amigoIngresado);
+    listaAmigos("#listaAmigos",amigoIngresado);
     console.log(amigos);
     limpiarCaja();
-
+    }
 }
 
 //Limpiar caja
 function limpiarCaja(){
     document.querySelector('#amigo').value ='';
 }
+
